f718ae0691c77a1a2ef74f2a2b1deda616b59551
Rob Wilkinson <1959628+RobAWilkinson@users.noreply.github.com>
2020-10-31 00:19:31 -0400

    parse emailll body

--- unabot/handler.js
+++ unabot/handler.js
@@ -1,11 +1,12 @@
 'use strict';
+const simpleParser = require('mailparser').simpleParser;
+
 
 module.exports.hello = async event => {
-  console.log(event)
-  const message = event.Records[0].Sns.Message;
-  console.log(message)
-
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  const message = JSON.parse(event.Records[0].Sns.Message);
+  const mail = message.mail;
+  const source = mail.source;
+  const content = message.content
+  let parsed = await simpleParser(content);
+  console.log(parsed.text);
 };
