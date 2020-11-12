e9005a5c2978058baaf0ff5d9658796fbb21e368
Rob Wilkinson <1959628+RobAWilkinson@users.noreply.github.com>
2020-11-12 17:30:29 -0500

    add message user functionality

--- unabot/slackClient.js
+++ unabot/slackClient.js
@@ -1,24 +1,22 @@
 const { WebClient } = require('@slack/web-api');
 const token = process.env.SLACK_BOT_TOKEN;
-
-const web = new WebClient(token);
+let web;
+if(process.env.NODE_ENV == "test") {
+    web = new WebClient(token, { slackApiUrl: "http://localhost:3000/api/"});
+} else {
+    web = new WebClient(token);
+}
 const emails = [
     "foo@bar.com"
 ]
 
 const lookupByName = async(name) => {
     const realName = name.replace(' ', '').split(',').reverse().join(' ');
-    console.log(realName);
     const users = await allUsers();
     const activeUsers = users.members.filter(user => !user.deleted);
-
-    console.log(activeUsers);
-
 }
 
 const lookupByEmail = async (emails) => {
-    try {
-
     const users = await Promise.all(
         emails.map(async (email) => {
             const user = await web.users.lookupByEmail({
@@ -27,15 +25,14 @@ const lookupByEmail = async (emails) => {
             return user;
         }))
         return users;
-        
-    } catch (error) {
-        if(error.code == "slack_webapi_platform_error") {
-            throw new Error("No user matching that email found");
-
-        }
-    }
-
 };
+const messageUser = async(user, message) => {
+    const resp = await web.chat.postMessage({
+        channel: user.id,
+        text: message
+    })
+    return resp;
+}
 
 const allUsers = async () => {
     let res = await web.users.list()
@@ -46,5 +43,6 @@ module.exports =
 {
     lookupByEmail,
     allUsers,
-    lookupByName
+    lookupByName,
+    messageUser
 }
--- unabot/test/slackClient_test.js
+++ unabot/test/slackClient_test.js
@@ -1,21 +1,33 @@
+require('dotenv').config()
+
 const { expect } = require('chai');
 const slackClient = require('../slackClient')
-
+const sampleUser = require('./user')
 
 describe('Lookup User', async () => {
     it('has a method to lookup a user by email', async () => {
         const emails = [
-            "rob@oddball.io"
+            "rob@oddball.io",
         ]
         const users = await slackClient.lookupByEmail(emails)
         expect(users.length).to.equal(1);
         expect(users[0].user.name).to.eql("rob")
     });
 
-    it('has a method to lookup a user by name', async () => {
-        const name = "Wilkinson, Robert";
-        const user = await slackClient.lookupByName(name);
-        expect(user.user.name).to.eql("rob")
+    xit('has a method to lookup 2 users by email', async () => {
+        const emails = [
+            "rob@oddball.io",
+            "dale.nelson@oddball.io"
+        ];
+        const users = await slackClient.lookupByEmail(emails)
+        expect(users.length).to.equal(2);
+        expect(users[0].user.name).to.eql("rob")
+        expect(users[1].user.name).to.eql("dale.nelson")
+    });
+    xit('sends a message to a specific user', async () => {
+        const message = `Please enter your hours\nhttps://oddball.unanet.biz/oddball/action/time/current`;
+        const resp = await slackClient.messageUser(sampleUser, message);
+        expect(resp.ok).to.be.true;
     })
 })
 
